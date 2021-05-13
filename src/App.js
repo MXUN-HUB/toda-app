10c5f6e58c4120e2084a2d207742bdaee49d51ab
MXUN-HUB <sharmamuditamb@gmail.com>
2021-05-13 14:14:52 +0530

    todo commit 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import React, { useState} from 'react';
 
 function App() {
+
+  const [Todo_item, setTodo_item] = useState(['a','b','c'])
+  const [Input_field, setInput_field] = useState('');
+
+  const add_todo = (new_item)=>{
+
+    new_item.preventDefault();
+    setTodo_item([...Todo_item,Input_field]);
+    setInput_field('');
+    
+  }
+
   return (
     <div className="App">
+    <h1>Todo-app</h1>
+    <form>
+    <input value={Input_field} onChange={new_item => setInput_field(new_item.target.value)}/>
+    <button type='submit' onClick={add_todo}>ADD</button>
+    </form>
+    <ul>
+    {Todo_item.map(item=> (
+      <li>{item}</li>
+    ))}
+    </ul>
       
     </div>
   );
